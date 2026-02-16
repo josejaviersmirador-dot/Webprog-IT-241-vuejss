f75d727c02d831c78c8721beeb45aa78dac3554d
Jose Javier Mirador <josejaviersmirador@gmail.com>
2026-02-16 23:14:21 +0000

    updated app.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
-  import PersonalProfile from './components/PersonalProfile.vue'
-  import Education from './components/Education.vue'
+import RestApi from './components/RestApi.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
+import Education from './components/Education.vue'
 </script>
 
 <template>
   <div class="portfolio-wrapper">
     <h1>My Portfolio</h1>
     
-    <PersonalProfile />
+    <RestApi />
+
+    <hr />
     
+    <PersonalProfile />
     <Education />
   </div>
 </template>
 
 <style scoped>
+hr {
+  margin: 40px 0;
+  border: 1px solid #444;
+}
 </style>
