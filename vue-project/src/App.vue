b64b9b62c6ae39fb842942e2092f9e0b69557d3f
Jose Javier Mirador <josejaviersmirador@gmail.com>
2026-02-16 23:25:39 +0000

    feat: display RestApi component on live site

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import RestApi from './components/RestApi.vue' // Add this line
+import RestApi from './components/RestApi.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 import Education from './components/Education.vue'
 </script>
@@ -8,7 +8,9 @@ import Education from './components/Education.vue'
   <div class="portfolio-wrapper">
     <h1>My Portfolio</h1>
     
-    <RestApi /> <PersonalProfile />
+    <RestApi />
+
+    <PersonalProfile />
     <Education />
   </div>
 </template>
